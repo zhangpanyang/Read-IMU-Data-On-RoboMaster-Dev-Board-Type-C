ebcde5e97f64e7102232a989b3d1597cf5b1df4f
zhangpanyang <azhangpanyang@126.com>
2024-11-02 15:51:02 +0800

    使用定时器，修改加热占空比

--- Core/Src/startup.cpp
+++ Core/Src/startup.cpp
@@ -4,10 +4,24 @@
 
 #include "startup.h"
 #include "imu.h"
+#include "tim.h"
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+	if (htim == &htim6)
+	{
+		BMI088ReadAccel();
+		BMI088ReadGyro();
+	}
+}
 
 void startup()
 {
 	BMI088Init();
+	HAL_TIM_Base_Start_IT(&htim6);
+	HAL_TIM_Base_Start(&htim10);
+	HAL_TIM_PWM_Start(&htim10, TIM_CHANNEL_1);
+	__HAL_TIM_SetCompare(&htim10, TIM_CHANNEL_1, 50);
 	while (1)
 	{
 
--- MDK-ARM/STM32ReadIMU/startup.d
+++ MDK-ARM/STM32ReadIMU/startup.d
@@ -34,4 +34,5 @@ stm32readimu/startup.o: ..\Core\Src\startup.cpp ..\Core\Inc\startup.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_pwr_ex.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_spi.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_tim.h \
-  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_tim_ex.h
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_tim_ex.h \
+  ..\Core\Inc\tim.h
